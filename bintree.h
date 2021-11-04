b6cef83c2801434b88a09de75dd39c8485d1193f
Logan Falkenberg <loganf0101@gmail.com>
2021-11-04 12:01:12 -0500

    Created all method prototypes

--- bintree.h
+++ bintree.h
@@ -9,9 +9,35 @@
 class BinTree{
 
     public:
+        // Constructor/Destructor
         BinTree();
+        ~BinTree();
 
+        // Public Methods
+        bool isEmpty();
+        int getCount();
+        bool getRootData(Data *);
+        void diplayTree();
 
+        void clear();
+        bool addNode(int, const string *);
+        bool removeNode(int);
+        bool getNode(Data *, int);
+        bool contains(int);
+        int getHeight();
+        void displayPreOrder();
+        void displayPostOrder();
+        void displayInOrder();
+
+        void clear(DataNode *);
+        bool addNode(DataNode *, DataNode *);
+        DataNode* removeNode(int, DataNode);
+        bool getNode(Data *, int, DataNode *);
+        bool contains(int, DataNode *);
+        int getHeight(DataNode *);
+        void displayPreOrder(DataNode *);
+        void displayPostOrder(DataNode *);
+        void displayInOrder(DataNode *);
 
     private:
         DataNode *root;
