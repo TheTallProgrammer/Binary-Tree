65c86cff5fe2b53419a3802eb1e2f4b3191afd17
Logan Falkenberg <loganf0101@gmail.com>
2021-11-04 12:11:53 -0500

    Created all stub methods

--- bintree.cpp
+++ bintree.cpp
@@ -4,4 +4,104 @@
 
 #include "bintree.h"
 
+// Constructor/Destructor
+BinTree::BinTree(){
+
+} // End of constructor
+
+BinTree::~BinTree(){
+
+} // End of destructor
+
+
+// Public methods
+bool BinTree::isEmpty(){
+
+} // End of isEmpty
+
+int BinTree::getCount(){
+
+} // End of getCount
+
+bool BinTree::getRootData(Data *){
+
+} // End of getRootData
+
+void BinTree::diplayTree(){
+
+} // End of displayTree
+
+void BinTree::clear() {
+
+} // End of clear
+
+bool BinTree::addNode(int, const string *){
+
+} // End of addNode
+
+bool BinTree::removeNode(int){
+
+} // End of removeNode
+
+bool BinTree::getNode(Data *, int){
+
+} // End of getNode
+
+bool BinTree::contains(int){
+
+} // End of contains
+
+int BinTree::getHeight(){
+
+} // End of getHeight
+
+void BinTree::displayPreOrder(){
+
+} // End of displayPreOrder
+
+void BinTree::displayPostOrder(){
+
+} // End of displayPostOrder
+
+void BinTree::displayInOrder(){
+
+} // End of displayInOrder
+
+
+// Overload methods
+void BinTree::clear(DataNode *){
+
+} // End of clear
+
+bool BinTree::addNode(DataNode *, DataNode *){
+
+} // End of addNode
+
+DataNode* BinTree::removeNode(int, DataNode){
+
+} // End of removeNode
+
+bool BinTree::getNode(Data *, int, DataNode *){
+
+} // End of getNode
+
+bool BinTree::contains(int, DataNode *){
+
+} // End of contains
+
+int BinTree::getHeight(DataNode *){
+
+} // End of getHeight
+
+void BinTree::displayPreOrder(DataNode *){
+
+} // End of displayPreOrder
+
+void BinTree::displayPostOrder(DataNode *){
+
+} // End of displayPostOrder
+
+void BinTree::displayInOrder(DataNode *){
+
+} // End of displayInOrder
 
